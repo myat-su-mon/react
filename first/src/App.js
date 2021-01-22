ca6ae8b79421a28817db83b2afbd8d54297c0a83
Myat Su Mon <msm@Myats-MacBook-Pro.local>
2021-01-22 22:02:20 +0630

    props method

--- first/src/App.js
+++ first/src/App.js
@@ -11,21 +11,77 @@ class Item extends React.Component {
     }
 }
 
-class App extends React.Component {
-    state = {
-        items: [
-            {id:1, name: 'Apple', price: 109},
-            {id:2, name: 'Orange', price: 0.59},
-        ]
-    }
+// class App extends React.Component {
+//     state = {
+//         items: [
+//             {id:1, name: 'Apple', price: 109},
+//             {id:2, name: 'Orange', price: 0.59},
+//         ]
+//     }
+//
+//     nameRef = React.createRef();
+//     priceRef = React.createRef();
+//
+//     add = () => {
+//         let id = this.state.items.length + 1;
+//         let name = this.nameRef.current.value;
+//         let price = this.priceRef.current.value;
+//
+//         this.setState({
+//             items: [
+//                 ...this.state.items,
+//                 {id, name, price}
+//             ]
+//         });
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Hello React</h1>
+//                 <ul>
+//                     {this.state.items.map(i => {
+//                         return (
+//                             <Item
+//                                 key = {i.id}
+//                                 name = {i.name}
+//                                 price = {i.price}
+//                                 />
+//                         )
+//                     })}
+//                 </ul>
+//                 <input type="text" ref={this.nameRef} /> <br/>
+//                 <input type="text" ref={this.priceRef} /> <br/>
+//                 <button onClick={this.add}>Add</button>
+//             </div>
+//         )
+//     }
+// }
 
+class AddForm extends React.Component {
     nameRef = React.createRef();
     priceRef = React.createRef();
 
-    add = () => {
+    render() {
+        return (
+            <div>
+                <input type="text" ref={this.nameRef} /><br/>
+                <input type="text" ref={this.priceRef} /><br/>
+                <button onClick={this.add}>Add</button>
+            </div>
+        )
+    }
+}
+
+class App extends React.Component {
+    state = {
+        items: [
+            {id: 1, name: 'Apple', price: 0.99},
+            {id: 2, name: 'Orange', price: 0.89},
+        ]
+    }
+
+    add = (name, price) => {
         let id = this.state.items.length + 1;
-        let name = this.nameRef.current.value;
-        let price = this.priceRef.current.value;
 
         this.setState({
             items: [
@@ -34,6 +90,7 @@ class App extends React.Component {
             ]
         });
     }
+
     render() {
         return (
             <div>
@@ -42,16 +99,14 @@ class App extends React.Component {
                     {this.state.items.map(i => {
                         return (
                             <Item
-                                key = {i.id}
+                                id = {i.id}
                                 name = {i.name}
                                 price = {i.price}
-                                />
+                            />
                         )
                     })}
                 </ul>
-                <input type="text" ref={this.nameRef} /> <br/>
-                <input type="text" ref={this.priceRef} /> <br/>
-                <button onClick={this.add}>Add</button>
+                <AddForm add={this.add}/>
             </div>
         )
     }
