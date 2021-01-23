c685328ddc038c3cfddfd48c7225d37ff59d7f2f
Myat Su Mon <myatsumon@ucsy.edu.mm>
2021-01-23 15:16:44 +0630

    ch10 react router

--- router/src/App.js
+++ router/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useParams,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const users = [
+//     {id: 1, name: 'Alice', gender: 'f'},
+//     {id: 2, name: 'Bob', gender: 'm'},
+//     {id: 3, name: 'Tom', gender: 'm'},
+//     {id: 4, name: 'Mary', gender: 'f'},
+// ];
+//
+// const Male = props => {
+//     return (
+//         <ul>
+//             {users.filter(u => u.gender === 'm')
+//                 .map(u => <li key={u.id}>{u.name}</li>)}
+//         </ul>
+//     );
+// }
+// const Female = props => {
+//     return (
+//         <ul>
+//             {users.filter(u => u.gender === 'f')
+//                 .map(u => <li key={u.id}>{u.name}</li>)}
+//         </ul>
+//     );
+// }
+//
+// const App = props => {
+//     return (
+//         <Router>
+//             <div>
+//                 <ul>
+//                     <li><Link to="/male">Male</Link></li>
+//                     <li><Link to="/female">Female</Link></li>
+//                 </ul>
+//                 <div style={{background: 'cyan', padding: 20}}>
+//                     <Switch>
+//                         <Route path="/male" exact><Male /></Route>
+//                         <Route path="/female"><Female /></Route>
+//                     </Switch>
+//                 </div>
+//             </div>
+//         </Router>
+//     );
+// }
+
+const User = props => {
+    const {name} = useParams();
+    return (
+        <h1>Profile - {name} </h1>
+    )
+}
+
+const App = props => {
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li><Link to="/user/Alice">Alice</Link></li>
+                    <li><Link to="/user/Bob">Bob</Link></li>
+                </ul>
+                <div style={{background: 'cyan', padding: 20}}>
+                    <Switch>
+                        <Route path="/user/:name"><User /></Route>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
